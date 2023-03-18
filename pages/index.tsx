981032808f0e3bab3896b07ab4d182b4855d0640
bsz90 <bsz90@users.noreply.github.com>
2023-03-17 20:15:46 -0400

    Add function to return chart based on instrument type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { SetState } from "immer/dist/internal";
+import { SetStateAction, useState } from "react";
+import { BrassFingeringChart } from "./BrassFingeringChart";
 import { NavBar } from "./NavBar";
 import { SingleReedFingeringChart } from "./SingleReedFingeringChart";
 import { Start } from "./Start";
-import { Instrument, WoodwindInstrument } from "./types";
+import { BrassInstrument, Instrument, WoodwindInstrument } from "./types";
 
 export default function Home() {
   const [currentFamily, setCurrentFamily] = useState("woodwind");
@@ -10,20 +12,38 @@ export default function Home() {
     WoodwindInstrument.SAXOPHONE
   );
 
+  const determineFingeringChart = (currentInstrument: Instrument) => {
+    if (
+      currentInstrument === WoodwindInstrument.BASSOON ||
+      WoodwindInstrument.OBOE ||
+      WoodwindInstrument.FLUTE ||
+      WoodwindInstrument.CLARINET ||
+      WoodwindInstrument.SAXOPHONE
+    ) {
+      return (
+        <SingleReedFingeringChart
+          currentInstrument={currentInstrument as WoodwindInstrument}
+          setCurrentInstrument={setCurrentInstrument}
+        />
+      );
+    }
+    if (
+      currentInstrument === BrassInstrument.TRUMPET ||
+      BrassInstrument.TROMBONE ||
+      BrassInstrument.FRENCH_HORN ||
+      BrassInstrument.TUBA
+    ) {
+      <BrassFingeringChart
+        currentInstrument={currentInstrument as BrassInstrument}
+        setCurrentInstrument={setCurrentInstrument}
+      />;
+    }
+    throw new Error();
+  };
+
   return (
     <div className="w-full h-screen flex flex-col bg-slate-200 items-center justify-start touch-none">
-      {currentInstrument ? (
-        <SingleReedFingeringChart
-          currentInstrument={currentInstrument}
-          setCurrentInstrument={setCurrentInstrument}
-        />
-      ) : (
-        <Start
-          currentFamily={currentFamily}
-          setCurrentFamily={setCurrentFamily}
-          setCurrentInstrument={setCurrentInstrument}
-        />
-      )}
+      {determineFingeringChart(currentInstrument)}
       <NavBar
         currentInstrument={currentInstrument}
         setCurrentInstrument={setCurrentInstrument}
